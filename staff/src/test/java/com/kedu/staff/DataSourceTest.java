e7b4f96d0fac09e79ae50e2e842d48b41a949a13
이영옥 <yolily7@naver.com>
2016-09-13 14:33:15 +0900

    Staff on Spring

--- staff/src/test/java/com/kedu/staff/DataSourceTest.java
+++ staff/src/test/java/com/kedu/staff/DataSourceTest.java
@@ -1,10 +1,8 @@
 package com.kedu.staff;
 
 import java.sql.Connection;
-
 import javax.inject.Inject;
 import javax.sql.DataSource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -25,7 +23,6 @@ public class DataSourceTest {
 		
 		try(Connection con = ds.getConnection() ) {
 			System.out.println(con);
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
